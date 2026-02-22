823aec86dc7918f8b41871d68b10d4ecc9718a1c
Silvia Alejandra Olaya Forero <salejandraolaya@gmail.com>
2026-02-22 14:40:18 -0500

    feat: design profile component with user details display and logout functionality

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { IUser } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: IUser | null = null;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.user = this.authService.getUser();
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
