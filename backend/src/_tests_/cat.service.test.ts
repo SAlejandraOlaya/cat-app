109a1983b262011b53186238536edf107a459f78
Silvia Alejandra Olaya Forero <salejandraolaya@gmail.com>
2026-02-22 09:14:50 -0500

    feat: add Jest configuration and initial tests for cat service functionality

---
+++ backend/src/_tests_/cat.service.test.ts
@@ -0,0 +1,52 @@
+import { getAllBreeds, getBreedById, searchBreeds } from '../services/cat.service';
+import { httpClient } from '../config/http.client';
+
+
+jest.mock('../config/http.client');
+
+describe('Cat Service', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    it('should return a list of breeds', async () => {
+        const mockBreeds = [
+            { id: 'abys', name: 'Abyssinian' },
+            { id: 'beng', name: 'Bengal' },
+        ];
+        (httpClient.get as jest.Mock).mockResolvedValue({ data: mockBreeds });
+        const result = await getAllBreeds();
+        expect(result).toEqual(mockBreeds);
+        expect(httpClient.get).toHaveBeenCalledWith('/breeds');
+        expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return a breed by id', async () => {
+        const mockBreed = { id: 'abys', name: 'Abyssinian' };
+        (httpClient.get as jest.Mock).mockResolvedValue({ data: mockBreed });
+
+
+        const result = await getBreedById('abys');
+
+
+        expect(result).toEqual(mockBreed);
+        expect(httpClient.get).toHaveBeenCalledWith('/breeds/abys');
+        expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
+
+
+    it('should return breeds matching a search query', async () => {
+
+        const mockBreeds = [{ id: 'beng', name: 'Bengal' }];
+        (httpClient.get as jest.Mock).mockResolvedValue({ data: mockBreeds });
+
+
+        const result = await searchBreeds('beng');
+
+
+        expect(result).toEqual(mockBreeds);
+        expect(httpClient.get).toHaveBeenCalledWith('/breeds/search?q=beng');
+        expect(httpClient.get).toHaveBeenCalledTimes(1);
+    });
+});
+
